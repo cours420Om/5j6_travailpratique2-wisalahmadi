0174aaa17075c433c82bd4c1cad633a11266384a
wisalahmadi <89399914+wisalahmadi@users.noreply.github.com>
2022-12-10 21:39:46 -0500

    J'ai ajouter 3 nouveau activités et j'ai coder l'interface de l'activityAjouter

--- app/src/main/java/com/example/db_tp2/ActivityGestionProfilDeconnexion.java
+++ app/src/main/java/com/example/db_tp2/ActivityGestionProfilDeconnexion.java
@@ -1,34 +1,80 @@
 package com.example.db_tp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.db_tp2.databinding.ActivityGestionProfilDeconnexionBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class ActivityGestionProfilDeconnexion extends AppCompatActivity {
 
-    EditText et_Editemail, et_nom;
-    Button btn_update;
-    TextView tv_bjr;
+    ActivityGestionProfilDeconnexionBinding binding;
+    String nom, courriel;
+    FirebaseAuth bd;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gestion_profil_deconnexion);
 
-        et_Editemail = findViewById(R.id.et_courriel);
-        et_nom = findViewById(R.id.et_name);
-        btn_update = findViewById(R.id.btn_update);
-        tv_bjr = findViewById(R.id.tv_titre2);
+        binding = ActivityGestionProfilDeconnexionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        btn_update.setOnClickListener(new View.OnClickListener() {
+        binding.btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                nom = binding.etName.getText().toString();
+                courriel = binding.etCourriel.getText().toString();
 
-                tv_bjr.setText("Bonjour " + et_nom.getText().toString());
+                if(!nom.isEmpty() && !courriel.isEmpty()){
+                    Utilisateur usager = new Utilisateur(nom,courriel);
+
+                    bd = FirebaseAuth.getInstance();
+                    ref = FirebaseDatabase.getInstance().getReference("Utilisateurs");
+                    ref.child(nom).setValue(usager).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            binding.etName.setText("");
+                            binding.etCourriel.setText("");
+                            Toast.makeText(ActivityGestionProfilDeconnexion.this,"Update!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+    private void majDonnees(String nom, String courriel) {
+
+        HashMap usagers = new HashMap();
+        usagers.put("Nom complet",nom);
+        usagers.put("Courriel",courriel);
+
+        ref = FirebaseDatabase.getInstance().getReference("Utilisateurs");
+        ref.child(nom).updateChildren(usagers).addOnCompleteListener(new OnCompleteListener() {
+
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()){
+
+                    binding.etName.setText("");
+                    binding.etCourriel.setText("");
+                    Toast.makeText(ActivityGestionProfilDeconnexion.this,"Updated Success",Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    Toast.makeText(ActivityGestionProfilDeconnexion.this,"Update fail",Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
--- app/src/main/java/com/example/db_tp2/Utilisateur.java
+++ app/src/main/java/com/example/db_tp2/Utilisateur.java
@@ -3,38 +3,16 @@ package com.example.db_tp2;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Utilisateur implements Parcelable {
+public class Utilisateur {
     private String nom;
     private String courriel;
-    private String pass;
 
-    public Utilisateur(String nom, String courriel, String pass, String passVer) {
+    public Utilisateur(String nom, String courriel) {
         this.nom = nom;
         this.courriel = courriel;
-        this.pass = pass;
-        this.passVer = passVer;
+
     }
 
-    private String passVer;
-
-    protected Utilisateur(Parcel in) {
-        nom = in.readString();
-        courriel = in.readString();
-        pass = in.readString();
-        passVer = in.readString();
-    }
-
-    public static final Creator<Utilisateur> CREATOR = new Creator<Utilisateur>() {
-        @Override
-        public Utilisateur createFromParcel(Parcel in) {
-            return new Utilisateur(in);
-        }
-
-        @Override
-        public Utilisateur[] newArray(int size) {
-            return new Utilisateur[size];
-        }
-    };
 
     public String getNom() {
         return nom;
@@ -52,32 +30,8 @@ public class Utilisateur implements Parcelable {
         this.courriel = courriel;
     }
 
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
-
-    public String getPassVer() {
-        return passVer;
-    }
-
-    public void setPassVer(String passVer) {
-        this.passVer = passVer;
-    }
-
     @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int i) {
-        parcel.writeString(nom);
-        parcel.writeString(courriel);
-        parcel.writeString(pass);
-        parcel.writeString(passVer);
+    public String toString(){
+        return nom + ", " + courriel ;
     }
 }
